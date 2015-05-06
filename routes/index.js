65ec4e7dcd9dbb391be57d32c24c89e504b7cb28
Duron Clemence <cduron@turing.u-strasbg.fr>
2015-05-06 16:37:46 +0200

    Dernières modifications du design de base

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', tweets: tweets });
 });
 
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
 router.post('/', function(req, res, next) {
   tweets.unshift(req.body.tweet);
   res.redirect('/')
