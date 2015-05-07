6c064f55a2d7bcd6705f214a9c21b9f257757b9c
Duron Clemence <cduron@turing.u-strasbg.fr>
2015-05-07 15:59:25 +0200

    Multiples modifications Ajout de l'heure Menu hamburger fonctionnel (petite largeur de fenêtre) Suppression du bug de duplication des tweets au rechargement

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var tweets = [];
 var comments = [];
+var previousTweet = {};
+var previousComment = {};
 
 /* Pages */
 router.get('/', function(req, res, next) {
@@ -24,21 +26,80 @@ router.get('/profile', function(req, res, next) {
 /* Tweets */
 
 router.post('/', function(req, res, next) {
-  tweets.unshift(req.body.tweet);
-  res.redirect('/')
+	if (req.body.tweet.trim() == "") {
+		res.redirect('/');
+		return;
+	}
+	var date = new Date().getTime();
+	if (previousTweet == {}) {
+		previousTweet = {text: req.body.tweet, date: date};
+		tweets.unshift({
+				text: req.body.tweet,
+				date: date
+		});
+	} else {
+		if (previousTweet.text != req.body.tweet) {
+			previousTweet = {text: req.body.tweet, date: date};
+			tweets.unshift({
+					text: req.body.tweet,
+					date: date
+			});
+		}
+	}
+
+	res.redirect('/');
 });
 
 router.post('/favorites', function(req, res, next) {
-  tweets.unshift(req.body.tweet);
-  res.redirect('favorites')
+	if (req.body.tweet.trim() == "") {
+		res.redirect('favorites');
+		return;
+	}
+	var date = new Date().getTime();
+	if (previousTweet == {}) {
+		previousTweet = {text: req.body.tweet, date: date};
+		tweets.unshift({
+				text: req.body.tweet,
+				date: date
+		});
+	} else {
+		if (previousTweet.text != req.body.tweet) {
+			previousTweet = {text: req.body.tweet, date: date};
+			tweets.unshift({
+					text: req.body.tweet,
+					date: date
+			});
+		}
+	}
+
+	res.redirect('favorites');
 });
 
 /* Comments */
 
 router.post('/minitwr', function(req, res, next) {
-  comments.unshift(req.body.comment);
-  res.redirect('minitwr')
-});
+	if (req.body.comment.trim() == "") {
+		res.redirect('minitwr');
+		return;
+	}
+	var date = new Date().getTime();
+	if (previousComment == {}) {
+		previousComment = {text: req.body.comment, date: date};
+		tweets.unshift({
+				text: req.body.comment,
+				date: date
+		});
+	} else {
+		if (previousComment.text != req.body.comment) {
+			previousComment = {text: req.body.comment, date: date};
+			tweets.unshift({
+					text: req.body.comment,
+					date: date
+			});
+		}
+	}
 
+	res.redirect('minitwr');
+});
 
 module.exports = router;
