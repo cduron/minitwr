efff5f4b5ab396232b3d7a1000cb8e520d605438
Endenmann <cyrielle.endenmann@etu.unistra.fr>
2015-05-07 11:47:42 +0200

    Ajout des pages About us, Favorites et Profile

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,43 @@ var express = require('express');
 var router = express.Router();
 
 var tweets = [];
+var comments = [];
 
-/* GET home page. */
+/* Pages */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', tweets: tweets });
 });
 
-router.get('/signup', function(req, res, next) {
-  res.render('signup');
+router.get('/minitwr', function(req, res, next) {
+  res.render('minitwr', { title: 'Express', comments: comments });
 });
+
+router.get('/favorites', function(req, res, next) {
+  res.render('favorites', { title: 'Express', tweets: tweets });
+});
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile');
+});
+
+/* Tweets */
 
 router.post('/', function(req, res, next) {
   tweets.unshift(req.body.tweet);
   res.redirect('/')
 });
+
+router.post('/favorites', function(req, res, next) {
+  tweets.unshift(req.body.tweet);
+  res.redirect('favorites')
+});
+
+/* Comments */
+
+router.post('/minitwr', function(req, res, next) {
+  comments.unshift(req.body.comment);
+  res.redirect('minitwr')
+});
+
 
 module.exports = router;
